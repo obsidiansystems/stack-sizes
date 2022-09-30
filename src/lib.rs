e2c134081df06a934dbc18a21d1ad54ccc052fe5
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-09-30 21:38:59 +0000

    hax.

--- src/lib.rs
+++ src/lib.rs
@@ -483,7 +483,7 @@ pub fn run(path: &Path) -> Result<(), failure::Error> {
             // 32-bit address space
             println!("address\t\tstack\tname");
 
-            for (addr, sym) in symbols.defined {
+            for (name, (addr, sym)) in BTreeMap::from_iter(symbols.defined.into_iter().map(|k, v| { (v.names().first(), (k, v)) })) {
                 if let (Some(name), Some(stack)) = (sym.names().first(), sym.stack()) {
                     println!(
                         "{:#010x}\t{}\t{}",
